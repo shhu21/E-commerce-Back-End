a295c25701f5ebe3b97f6005509c04d1106d1faa
shhu21 <hu.sara21@gmail.com>
2020-08-12 19:59:20 -0700

    sets up table schemas and adds template route functionality

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      { model: Category },
+      { model: Tag }
+    ]
+  }).then(result => res.json(result));
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id).then(result => res.json(result));
 });
 
 // create new product
@@ -91,6 +98,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(result => res.json(result));
 });
 
 module.exports = router;
